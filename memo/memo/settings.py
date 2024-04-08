922c6b92dfb9f4d4ea5c67028d4c6de57a9eb242
unknown <yuu.okd.hotmail@gmail.com>
2024-04-08 20:33:09 +0900

    vercel対応5

--- memo/memo/settings.py
+++ memo/memo/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'memo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-DATABASES = {}
+#DATABASES = {}
 
 
 # Password validation
